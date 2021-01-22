4292742dda690f81c0a3d3b469b6880dcff99aa9
cinderblox94 <60583920+cinderblox94@users.noreply.github.com>
2021-01-22 12:45:19 +0100

    resuelto movimiento, queda animación y comprobación

--- 6-7.Ej 6-7/colisiones.js
+++ 6-7.Ej 6-7/colisiones.js
@@ -1,20 +1,26 @@
 function main(){
-    var numpx = document.getElementById("mov").value;
+    
     var bloque = document.querySelector(".block");
 
-    function derecha(){
-       var valor = bloque.style.left;
+    function mover(orientacion, direccion){
+       var numpx = parseInt(document.getElementById("mov").value);
+       var valor = parseInt(getComputedStyle(bloque).getPropertyValue(orientacion));
+       console.log(numpx);
        console.log(valor);
-     bloque.style.left = (valor + numpx);
+       bloque.style.setProperty(orientacion, ((valor + (numpx*direccion) + "px")));
+        console.log(parseInt(getComputedStyle(bloque).getPropertyValue(orientacion)));
     }
    function izquierda(){
+       var numpx = document.getElementById("mov").value;
         var valor = bloque.style.left;
         console.log(valor);
-     bloque.style.left = (valor - numpx);
+     bloque.style.left = (valor - numpx)+"px";
      }
 
-    document.getElementById("right").addEventListener("click",derecha);
-    document.getElementById("left").addEventListener("click",izquierda);
+    document.getElementById("right").addEventListener("click",() => mover("left",1));
+    document.getElementById("left").addEventListener("click",() => mover("left",-1));
+    document.getElementById("top").addEventListener("click",() => mover("top",-1));
+    document.getElementById("bottom").addEventListener("click",() => mover("top",1));
 }
 
 document.addEventListener("DOMContentLoaded", main);
