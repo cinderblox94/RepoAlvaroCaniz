30ae838d9505608fc2fccd35c3d2f42fbe1e73da
cinderblox94 <60583920+cinderblox94@users.noreply.github.com>
2021-02-02 13:00:38 +0100

    avance

---
+++ 4. Ej4/my-jQuery.js
@@ -0,0 +1,53 @@
+function slideUp(elemento, tiempo){
+     alturaOriginal = parseInt(getComputedStyle(elemento).height);
+
+    var restante = null;
+    function slide(timestamp){
+        if(!restante){
+            restante = timestamp;
+        }
+        let alturaActual = null;
+
+        if(tiempo == 0){
+            alturaActual = 0;
+        }else{
+            alturaActual = alturaOriginal - (((timestamp - restante)/tiempo)*alturaOriginal);
+            elemento.style.height = alturaActual+"px";
+        }
+
+        if(alturaActual > 0){//si está creciendo ejecuta slide
+            requestAnimationFrame(slide);
+        }else{//si está con altura 0, fuerzo el formato original
+            elemento.style.height = alturaOriginal + "px";
+            elemento.style.display = "none";
+        }
+    }
+    requestAnimationFrame(slide);
+}
+
+function slideDown(elemento, tiempo){
+     alturaOriginal = parseInt(getComputedStyle(elemento).height);
+
+    var restante = null;
+    function slide(timestamp){
+        if(!restante){
+            restante = timestamp;
+        }
+        let alturaActual = null;
+
+        if(tiempo == 0){
+            alturaActual = alturaOriginal;
+        }else{
+            alturaActual =((timestamp - restante)/tiempo)*alturaOriginal;
+            elemento.style.height = alturaActual+"px";
+        }
+
+        if(alturaActual <alturaOriginal){//si está creciendo ejecuta slide
+            requestAnimationFrame(slide);
+        }else{//si está con altura 0, fuerzo el formato original
+            elemento.style.height = alturaOriginal + "px";
+            elemento.style.display = "none";
+        }
+    }
+    requestAnimationFrame(slide);
+}
