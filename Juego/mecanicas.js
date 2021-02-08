657dfa4335ffa9513bcaabb1bb6577524020a2fa
cinderblox94 <60583920+cinderblox94@users.noreply.github.com>
2021-02-08 19:27:28 +0100

    hech salto y desplazamiento

--- Juego/mecanicas.js
+++ Juego/mecanicas.js
@@ -1,13 +1,18 @@
 const TIEMPODESAPARECE = 1000;
 const TIEMPOSALTO = 300;
 const ALTURASALTADA = 200;
-const ALTURACAIDA= 30;
-const TIEMPOCAIDA= 5000;
+const ALTURACAIDA = -150;
+const TIEMPOCAIDA = 1000;
+
+const TIEMPOTUBERIA =2000;
 
 document.addEventListener("DOMContentLoaded", ()=>{
+    const ANCHURATUBERIA = parseInt(getComputedStyle(document.querySelector(".tuberia")).width)
+    const DISTANCIATUBERIA = parseInt(getComputedStyle(document.querySelector("#ventana")).width) + ANCHURATUBERIA;
     var comienzo = false;
     var muerto = false;
     var jugador = document.querySelector("#jugador"); 
+    var tuberia = document.querySelector(".tuberia");
 
     //-----------MENU-----------------
     function ocultar(){//parametrizar para usar con pantalla fin para reiniciar
@@ -32,47 +37,79 @@ document.addEventListener("DOMContentLoaded", ()=>{
     }
 
     //-------------JUEGO-------------------
-    function salto(){
-        vuelo(ALTURASALTADA,TIEMPOSALTO);
-    }
-    function vuelo(cantidad, tiempo){//ahora mismo vale para subir como para bajar
-        var alturaActual = parseInt(getComputedStyle(jugador).bottom);
-        console.log(alturaActual);
-        var inicio = null;
-        function animacion(timestamp){
-            let acumulado = 0;
-            if(!inicio){
-                inicio = timestamp;
-            }
-            acumulado = timestamp - inicio;
-            let desplazado = ((ALTURASALTADA*acumulado)/tiempo);
-            jugador.style.setProperty("bottom", (alturaActual + desplazado + "px"));
-
-            if(acumulado <tiempo){
-                requestAnimationFrame(animacion)
-            }else{
-                jugador.style.setProperty("bottom",(alturaActual + cantidad+"px"));
-            }
-        };
-        requestAnimationFrame(animacion); 
-    }
-
-
-    //necesito una función que todo el rato me tire para abajo que se ejecute
-    //quizas un set INtervarl que se ejecute cuando no se salta , si se pulsa salta se para con clear interval, se salta y al acabar se vuelve a hacer la caida
-    // while(!muerto){
-    //     vuelo(ALTURACAIDA,TIEMPOCAIDA);
-    // }
-    //
     
-document.getElementById("botonTutorial").addEventListener("click",ocultar);
-window.addEventListener("keydown",salto);
+    function iniciar(){
+
+        function salto(){
+            vuelo(ALTURASALTADA,TIEMPOSALTO);
+           
+        }
+         
+        function vuelo(cantidad, tiempo){//ahora mismo vale para subir como para bajar
+            let alturaActual = parseInt(getComputedStyle(jugador).bottom);
+            let inicio = null;
+
+            function animacion(timestamp){
+                let acumulado = 0;
+                if(!inicio){
+                    inicio = timestamp;
+                }
+                acumulado = timestamp - inicio;
+                let desplazado = ((cantidad*acumulado)/tiempo);
+                jugador.style.setProperty("bottom", (alturaActual + desplazado + "px"));
+    
+                if(acumulado <tiempo){
+                    requestAnimationFrame(animacion)
+                }else{
+                    jugador.style.setProperty("bottom",(alturaActual + cantidad+"px"));
+                    caida();
+                }
+            };
+            requestAnimationFrame(animacion); 
+
+            if(comienzo ==false){
+                movTuberia(tuberia);
+                comienzo = true;
+            }
+        }
+
+        function caida(){
+           vuelo(ALTURACAIDA,TIEMPOCAIDA);
+           console.log(ALTURACAIDA>0);
+        }
+    
+        //DESARROLLO AUTOMÁTICO DEL JUEGO SIN INTERACCION----
+
+        //tiene un despazamiento por cada tubería
+        function movTuberia(tubo){
+            let posActual = parseInt(getComputedStyle(tubo).left);
+            let inicio = null;
+
+            function animacion(timestamp){
+                let acumulado = 0;
+                if(!inicio){
+                    inicio = timestamp;
+                }
+                acumulado = timestamp - inicio;
+                let desplazado = ((DISTANCIATUBERIA*acumulado)/TIEMPOTUBERIA);
+                tubo.style.setProperty("left", (posActual - desplazado +"px"));
+
+                if(acumulado < TIEMPOTUBERIA){
+                    requestAnimationFrame(animacion);
+                }else{
+                    tubo.style.setProperty("left",(posActual - DISTANCIATUBERIA));
+                }
+            };
+            requestAnimationFrame(animacion);
+        }
+        window.addEventListener("keydown",salto);
+    }
 
 
-
-
-
-
-
-
+    
+    
+    
+document.getElementById("botonTutorial").addEventListener("click",()=>{
+    ocultar();
+    iniciar()});
 });
